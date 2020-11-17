73c23f8a7192d7b2e39c8302b872b9a982fbb641
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-11-17 14:12:53 -0500

    products page built out at min level, addToCart working but needs refactor

--- src/features/products/buyerProducts/BuyerProductPage.jsx
+++ src/features/products/buyerProducts/BuyerProductPage.jsx
@@ -1,15 +1,44 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
 import Header from "../../shared/Header";
 import BProductCard from "./components/BProductCard";
 
+const Page = styled.div`
+	display: flex;
+	flex-flow: column nowrap;
+`;
+
+const Banner = styled.div`
+	margin: 3rem 0;
+	width: 100vw;
+	background-color: var(--pDark);
+	height: 10rem;
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: space-evenly;
+	align-items: center;
+	h1 {
+		display: block;
+		color: var(--pText);
+		font-size: 2rem;
+		text-decoration: none;
+	}
+`;
 const BuyerProductPage = props => {
-	
+	const products = useSelector(state => state.buyerProduct.products);
 	return (
-		<>
+		<Page>
 			<Header />
-			<div>Buyer Products Page</div>
-			<BProductCard />
-		</>
+			<Banner>
+				<h1>Buyer Products Page</h1>
+			</Banner>
+			{
+				products.map(product => {
+					return <BProductCard product={product} />
+				})
+			}
+		</Page>
 	);
 }
 
