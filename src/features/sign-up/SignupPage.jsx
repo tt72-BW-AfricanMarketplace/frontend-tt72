0f90ab7d04577a69a20d957704e6375b62af096c
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-11-17 15:03:16 -0500

    change form & flesh out product redux

--- src/features/sign-up/SignupPage.jsx
+++ src/features/sign-up/SignupPage.jsx
@@ -1,43 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../shared/Header"
 // import "./signup.css"
 import styled from "styled-components"
-import Form from "./SignupForm";
+import BuyerSignup from "./BuyerSignup";
+import OwnerSignup from "./OwnerSignup";
 import layout from "../layout"
 
 
-const { Container } = layout
+const { Container, Button } = layout
 
 const CustomHeader = styled(Container)`
     background-color: var(--pDarker);
 	width: 90%;
 	border-radius: 20px;
 	text-align: center;
-
 `;
-const signUpValues = [
-    {
-        name: "email",
-        type: "email",
-        placeholder: "email",
-        className: "email-field",
-    },
-    {
-        name: "password",
-        type: "password",
-        placeholder: "password",
-        className: "password-field",
-    }
-];
+
+// const signUpValues = [
+// 	{
+// 		name: "email",
+// 		type: "email",
+// 		placeholder: "email",
+// 		className: "email-field",
+// 	},
+// 	{
+// 		name: "password",
+// 		type: "password",
+// 		placeholder: "password",
+// 		className: "password-field",
+// 	}
+// ];
 const SignupPage = props => {
-    return (
-        <>
-            <Header className="cust" />
-            <CustomHeader>
-                <Form fields={signUpValues} />
-            </CustomHeader>
-        </>
-    )
+	const [isBuyer, setIsBuyer] = useState(false);
+
+	const toggle = () => {
+		setIsBuyer(!isBuyer);
+	}
+	
+	return (
+		<>
+			<Header className="cust" />
+			<Button onClick={toggle}>Switch</Button>
+			<CustomHeader>
+				{
+					isBuyer
+						? <BuyerSignup />
+						: <OwnerSignup />
+				}
+
+			</CustomHeader>
+		</>
+	)
 }
 
 export default SignupPage
